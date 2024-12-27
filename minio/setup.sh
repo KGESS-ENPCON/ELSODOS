cdecb0a838b57b970a6547eb54e2255b2908932d
Valentin Binotto <72608683+valentinbinotto@users.noreply.github.com>
2024-12-27 01:15:34 +0100

    Update setup.sh

--- minio/setup.sh
+++ minio/setup.sh
@@ -10,7 +10,6 @@ mkdir -p /mnt/s3-data
 mount /dev/sda1 /mnt/s3-data
 echo '/dev/sda1 /mnt/s3-data ext4 defaults 0 2' | tee -a /etc/fstab
 
-mkdir /mnt/s3-data
 wget https://dl.min.io/server/minio/release/linux-arm64/minio_20241218131544.0.0_arm64.deb
 dpkg -i minio_20241218131544.0.0_arm64.deb
 MINIO_ROOT_USER=admin MINIO_ROOT_PASSWORD=password minio server /mnt/s3-data --console-address ":9001"
